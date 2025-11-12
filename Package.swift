e886b0718c8feacb477e01d8970e33aad597bbf3
Rudrank Riyam <rudrankriyam@gmail.com>
2025-11-12 12:30:30 +0530

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -11,8 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.25.2"),
-        .package(url: "https://github.com/ml-explore/mlx-swift-examples.git", branch: "main"),
+        .package(url: "https://github.com/ml-explore/mlx-swift-lm.git", branch: "main"),
         .package(url: "https://github.com/huggingface/swift-transformers", .upToNextMinor(from: "1.1.0")),
     ],
     targets: [
@@ -23,11 +22,8 @@ let package = Package(
         .target(
             name: "MLXAudio",
             dependencies: [
-                .product(name: "MLX", package: "mlx-swift"),
-                .product(name: "MLXNN", package: "mlx-swift"),
-                .product(name: "MLXRandom", package: "mlx-swift"),
-                .product(name: "MLXLMCommon", package: "mlx-swift-examples"),
-                .product(name: "MLXLLM", package: "mlx-swift-examples"),
+                .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
+                .product(name: "MLXLLM", package: "mlx-swift-lm"),
                 .product(name: "Transformers", package: "swift-transformers"),
                 "ESpeakNG"
             ],
